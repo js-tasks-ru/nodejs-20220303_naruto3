d64976d63c8dcd10bc205ad0e2a310c51cfe552b
Ilya Chernyaev <iachernyaev@avito.ru>
2022-03-23 21:16:21 +0300

    03-01 fix for encodings

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -5,15 +5,16 @@ class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
     this.limit = options.limit;
+    this.encoding = options.encoding;
     this.dataSize = 0;
   }
 
   _transform(chunk, encoding, callback) {
-    this.dataSize += chunk.length;
+    this.dataSize += Buffer.byteLength(chunk, this.encoding);
     if (this.dataSize > this.limit) {
       callback(new LimitExceededError(), null);
     } else {
-      callback(null, chunk);
+      callback(null, chunk.toString(this.encoding));
     }
   }
 }
